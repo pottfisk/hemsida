32a0c71b8a1b4abb8d6a61c85b7649ce7da57b7d
pottfisk <robert.paananen@hotmail.com>
2022-01-31 17:32:12 +0100

    cookie

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const container = document.querySelectorAll(".dark");
-if(getCookie("darkModeToggled") == "false" || "darkModeToggled") == ""){
+if(getCookie("darkModeToggled") == "false" || getCookie("darkModeToggled") == ""){
     container.forEach(element => {element.classList.toggle("dark");});    
 }
 function toggleDark(){
